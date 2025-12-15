5eb14be3484d32c2cd321b924635211a96ab6b25
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-12-15 19:25:37 -0300

    actualizando api

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -8,7 +8,7 @@ export const AdminProvider = ({ children }) => {
     const [open, setOpen] = useState(false)
     const [seleccionado, setSeleccionado] = useState(null)
     const [openEditor, setOpenEditor] = useState(false)
-    const apiUrl = 'http://localhost:8080/products'
+    const apiUrl = 'https://codigo-back-production.up.railway.app/'
 
 
     useEffect(() => {
